6c35c0df617ec067ac94e0643205ce85d25ee13f
ahirapw-code <andhinihirapradipta@gmail.com>
2026-07-14 10:59:59 +0700

    Delete api directory

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { createApp } from "../server/app.js";
-
-// Vercel treats this exported Express app as a serverless function handler.
-// All requests to /api/* (see vercel.json) are routed here.
-const app = createApp();
-
-export default app;
